410b664bfd9f4309d63a4232d089c3e6411aa43d
nhd54 <nicklasdegnebolig@gmail.com>
2018-09-14 10:36:11 +0200

    working studentlist 1991

--- frontend.js
+++ frontend.js
@@ -3,71 +3,112 @@
 window.addEventListener("DOMContentLoaded", initFrontend);
 
 function initFrontend() {
-    console.log("Frontend is running");
+  console.log("Frontend is running");
 
-    // register buttons for sort
-    document.querySelector("button#sort_first").addEventListener("click", clickedSortFirstname );
-    document.querySelector("button#sort_last").addEventListener("click", clickedSortLastname );
-    document.querySelector("button#sort_house").addEventListener("click", clickedSortHouse );
+  // register buttons for sort
+  document
+    .querySelector("button#sort_first")
+    .addEventListener("click", clickedSortFirstname);
+  document
+    .querySelector("button#sort_last")
+    .addEventListener("click", clickedSortLastname);
+  document
+    .querySelector("button#sort_house")
+    .addEventListener("click", clickedSortHouse);
 
-    // register buttons for filters
-//    document.querySelectorAll("#filters a").forEach( function(element) { element.addEventListener("click", clickedFilter); } );
-    document.querySelectorAll("#filters a").forEach( element => element.addEventListener("click", clickedFilter));
+  // register buttons for filters
+  //    document.querySelectorAll("#filters a").forEach( function(element) { element.addEventListener("click", clickedFilter); } );
+  document
+    .querySelectorAll("#filters a")
+    .forEach(element => element.addEventListener("click", clickedFilter));
+}
+
+function clickTable() {
+  console.log("clicked table");
+  console.log(event.target);
+
+  const clicked = event.target;
+
+  if (clicked.tagName === "BUTTON") {
+    clickedDelete(clicked);
+  }
+}
+
+function clickedDelete(deleteButton) {
+  console.log(deleteButton);
+
+  let tr = deleteButton.parentElement;
+
+  while (tr.tagName !== "TR") {
+    tr = tr.parentElement;
+  }
+
+  const studentId = tr.dataset.studentId;
+  console.log(studentId);
+
+  deleteStudent(studentId);
+
+  tr.remove();
 }
 
 function clickedSortFirstname() {
-    console.log("clickedSortFirstname");
-    sortByFirstName();
-    displayList( currentStudents );
+  console.log("clickedSortFirstname");
+  sortByFirstName();
+  displayList(currentStudents);
 }
 
 function clickedSortLastname() {
-    console.log("clickedSortLastname");
-    sortByLastName();
-    displayList( currentStudents );
+  console.log("clickedSortLastname");
+  sortByLastName();
+  displayList(currentStudents);
 }
 
 function clickedSortHouse() {
-    console.log("clickedSortHouse");
-    sortByHouse();
-    displayList( currentStudents );
+  console.log("clickedSortHouse");
+  sortByHouse();
+  displayList(currentStudents);
 }
 
 function clickedFilter(event) {
-    console.log("clickedFilter");
-    const filter = this.dataset.filter; // references data-filter="____"
-    console.log(event);
-    event.preventDefault();
+  console.log("clickedFilter");
+  const filter = this.dataset.filter; // references data-filter="____"
+  console.log(event);
+  event.preventDefault();
 
-    // create a list of filtered students by house
+  // create a list of filtered students by house
 
-    // if filter === all, let the list be all students
-    if( filter === "all" ) {
-        currentStudents = allStudents;
-        displayList( currentStudents );
-    } else {
-        currentStudents = filterByHouse( filter );
-        displayList( currentStudents );
-    }
+  // if filter === all, let the list be all students
+  if (filter === "all") {
+    currentStudents = allStudents;
+    displayList(currentStudents);
+  } else {
+    currentStudents = filterByHouse(filter);
+    displayList(currentStudents);
+  }
 }
 
-function displayList( listOfStudents ) {
-    console.log("Display list");
-    // clear the table
-    document.querySelector("table#studentlist tbody").innerHTML = "";
+function displayList(listOfStudents) {
+  console.log("Display list");
+  // clear the table
+  document.querySelector("table#studentlist tbody").innerHTML = "";
 
-    // foreach student in listOfStudents
-    listOfStudents.forEach( function( student ) {
-        // clone a table-row for student
-        const clone = document.querySelector("#student_template").content.cloneNode(true);
-        
-        // fill in the clone with data
-        clone.querySelector("[data-firstname]").textContent = student.firstName;
-        clone.querySelector("[data-lastname]").textContent = student.lastName;
-        clone.querySelector("[data-house]").textContent = student.house;
+  // foreach student in listOfStudents
+  listOfStudents.forEach(function(student) {
+    // clone a table-row for student
+    const clone = document
+      .querySelector("#student_template")
+      .content.cloneNode(true);
 
-        // append clone to table
-        document.querySelector("table#studentlist tbody").appendChild( clone );
-    })
+    // fill in the clone with data
+    clone.querySelector("[data-firstname]").textContent = student.firstName;
+    clone.querySelector("[data-lastname]").textContent = student.lastName;
+    clone.querySelector("[data-house]").textContent = student.house;
+    clone.querySelector("tr").dataset.studentId = student.id;
 
+    document
+      .querySelector("table#studentlist")
+      .addEventListener("click", clickTable);
+    // append clone to table
+    document.querySelector("table#studentlist tbody").appendChild(clone);
+  });
 }
--- script.js
+++ script.js
@@ -6,21 +6,21 @@ const allStudents = [];
 let currentStudents = [];
 
 const Student_prototype = {
-    firstName: "",
-    lastName: "",
-    house: "",
-    toString() {
-        return this.firstName+" "+this.lastName;
-    },
-    splitName(fullName) {
-        const firstSpace = fullName.indexOf(" ");
-        this.firstName = fullName.substring(0,firstSpace);
-        this.lastName = fullName.substring(firstSpace+1);
-    },
-    setHouse(house) {
-        this.house = house;
-    }
-}
+  firstName: "",
+  lastName: "",
+  house: "",
+  toString() {
+    return this.firstName + " " + this.lastName;
+  },
+  splitName(fullName) {
+    const firstSpace = fullName.indexOf(" ");
+    this.firstName = fullName.substring(0, firstSpace);
+    this.lastName = fullName.substring(firstSpace + 1);
+  },
+  setHouse(house) {
+    this.house = house;
+  }
+};
 /*
 function init( ) {
 
@@ -56,156 +56,155 @@ function filterByHouse( house ) {
 }
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function init() {
-    // clear the students array - just in case
-    allStudents.splice(0, allStudents.length); // from https://stackoverflow.com/questions/1232040/how-do-i-empty-an-array-in-javascript
-    
-    // fetch JSON 
-    fetchData();
+  // clear the students array - just in case
+  allStudents.splice(0, allStudents.length); // from https://stackoverflow.com/questions/1232040/how-do-i-empty-an-array-in-javascript
 
-    // parse JSON
-    // --- done via fetchJSON
+  // fetch JSON
+  fetchData();
+
+  // parse JSON
+  // --- done via fetchJSON
 }
 
 function fetchData() {
-    const url= "students.json";
+  const url = "students.json";
 
-    fetch(url)
+  fetch(url)
     .then(function(response) {
-        return response.json();
+      return response.json();
     })
     .then(function(jsondata) {
-        console.log(jsondata);
+      console.log(jsondata);
 
-        buildList(jsondata); // creates allStudents
+      buildList(jsondata); // creates allStudents
 
-        currentStudents = allStudents;
-        
-        displayList(currentStudents);
+      currentStudents = allStudents;
+
+      displayList(currentStudents);
     });
 }
-
 
 function buildList(jsondata) {
-    // find the keys - expect them to be the houses
-    const houses = Object.keys(jsondata);
-    //console.log(houses);
-    // for each house, 
-    houses.forEach((house) => {
-        // fill the house with a list of students
-        const houseStudentNames = jsondata[house];
-        // console.log(houseStudentNames);
-        fillHouseWithStudents(house, houseStudentNames);
-    });
+  // find the keys - expect them to be the houses
+  const houses = Object.keys(jsondata);
+  //console.log(houses);
+  // for each house,
+  houses.forEach(house => {
+    // fill the house with a list of students
+    const houseStudentNames = jsondata[house];
+    // console.log(houseStudentNames);
+    fillHouseWithStudents(house, houseStudentNames);
+  });
 
-    function fillHouseWithStudents( house, studentNames ) {
-        studentNames.forEach( createStudent );
-    
-        function createStudent( fullName ) {
-            const student = Object.create(Student_prototype);
-            student.splitName(fullName);
-            //student.setHouse(house);
-            student.house = house;
-            allStudents.push(student);
-        }
+  function fillHouseWithStudents(house, studentNames) {
+    studentNames.forEach(createStudent);
+
+    function createStudent(fullName) {
+      const student = Object.create(Student_prototype);
+      student.splitName(fullName);
+      //student.setHouse(house);
+      student.house = house;
+
+      student.id = "" + allStudents.length;
+
+      allStudents.push(student);
     }
+  }
 }
 
-
 function sortByFirstName() {
-    currentStudents.sort( byFirstName );
+  currentStudents.sort(byFirstName);
 
-    function byFirstName(a,b) {
-        if( a.firstName < b.firstName ) {
-            return -1;
-        } else if( a.firstName > b.firstName ) {
-            return 1;
-        } else {
-            return 0;
-        }
+  function byFirstName(a, b) {
+    if (a.firstName < b.firstName) {
+      return -1;
+    } else if (a.firstName > b.firstName) {
+      return 1;
+    } else {
+      return 0;
     }
+  }
 }
 
 function sortByLastName() {
-    currentStudents.sort( byLastName );
+  currentStudents.sort(byLastName);
 
-    function byLastName(a,b) {
-        if( a.lastName < b.lastName ) {
-            return -1;
-        } else {
-            return 1;
-        }
+  function byLastName(a, b) {
+    if (a.lastName < b.lastName) {
+      return -1;
+    } else {
+      return 1;
     }
+  }
 }
 
+function deleteStudent(studentId) {
+  console.log("break here");
 
-
+  // function that returns true when student.id = studentId
+  const index = allStudents.findIndex(findStudent);
+  allStudents.splice(index, 1);
+  function findStudent(student) {
+    if (student.id === studentId) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+function generateUUID() {
+  // Public Domain/MIT
+  var d = new Date().getTime();
+  if (
+    typeof performance !== "undefined" &&
+    typeof performance.now === "function"
+  ) {
+    d += performance.now(); //use high-precision timer if available
+  }
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
+}
 function sortByHouse() {
-    currentStudents.sort( byHouseAndFirstName );
+  currentStudents.sort(byHouseAndFirstName);
 
-    function byHouseAndFirstName(a,b) {
-        // first sort by house, but if house is the same, sort by first name
-        if( a.house < b.house ) {
-            return -1;
-        } else if( a.house > b.house ) {
-            return 1;
-        } else {
-            if( a.firstName < b.firstName ) {
-                return -1;
-            } else {
-                return 1;
-            }
-        }
+  function byHouseAndFirstName(a, b) {
+    // first sort by house, but if house is the same, sort by first name
+    if (a.house < b.house) {
+      return -1;
+    } else if (a.house > b.house) {
+      return 1;
+    } else {
+      if (a.firstName < b.firstName) {
+        return -1;
+      } else {
+        return 1;
+      }
     }
+  }
 }
 
-function filterByHouse( house ) {
-    const filteredStudents = allStudents.filter( byHouse );
+function filterByHouse(house) {
+  const filteredStudents = allStudents.filter(byHouse);
 
-    function byHouse( student ) {
-        if( student.house === house ) {
-            return true;
-        } else {
-            return false;
-        }
+  function byHouse(student) {
+    if (student.house === house) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    return filteredStudents;
+  return filteredStudents;
 }
 
 function listOfStudents() {
-    let str = "";
+  let str = "";
 
-    allStudents.forEach( student => str+=student+"\n" );
+  allStudents.forEach(student => (str += student + "\n"));
 
-    return str;
+  return str;
 }
